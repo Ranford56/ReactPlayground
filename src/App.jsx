a9dc60570d86cafe742f4c744b493a2dcfe37a02
Ranford56 <rgarciac5@unemi.edu.ec>
2023-01-17 08:03:01 -0500

    funcionalidad a los botones de operaciones añadida

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Button from "./components/Button";
+import React, { useState } from "react";
 import Result from "./components/Result";
 import MathOperations from "./components/MathOperations";
 import Functions from "./components/Functions";
@@ -7,29 +6,38 @@ import Numbers from "./components/Numbers";
 import "./App.css";
 
 const App = () => {
+  const [texto, setTexto] = useState("");
   console.log("Renderizacion de app");
   return (
     <main className="react-calculator">
-      <Result value={"0"} />
+      <Result value={texto} />
       <Numbers
         onClickNumber={(number) => {
           console.log("number ", number);
+          setTexto(texto + number);
         }}
       ></Numbers>
       <Functions
         onContentClear={(clear) => {
-          console.log("equal ", clear);
+          console.log("delete all ", clear);
+          setTexto("");
         }}
         onDelete={(del) => {
-          console.log("delete ", del);
+          if (texto.length > 0) {
+            console.log("delete ", del);
+            const textoNuevo = texto.substring(0, texto.length - 1);
+            setTexto(textoNuevo);
+          }
         }}
       ></Functions>
       <MathOperations
         onClickOperation={(operation) => {
           console.log("operacion: ", operation);
+          setTexto(texto + operation);
         }}
         onClikcEqual={(equal) => {
           console.log("equal: ", equal);
+          setTexto(texto + equal);
         }}
       ></MathOperations>
     </main>
